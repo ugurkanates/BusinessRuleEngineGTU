2a0378d23282bfd1243e25c841a1305092313178
cantelek <cantelek@gmail.com>
2018-12-18 17:18:19 +0300

    relativeParser method is updated

--- RelativeChecker.java
+++ RelativeChecker.java
@@ -31,7 +31,7 @@ public class RelativeChecker{
 		for (int i = 0; i < rule.length(); ++i) {
 			if(isNumber(rule.charAt(i))){
 				String currentRelative = Character.toString(rule.charAt(i));
-				/* <= kontrol et */
+				
 				int j;
 				for (j = i+1; isNumber(rule.charAt(j)) == true && j <= rule.length(); ++j) {
 					currentRelative += (rule.charAt(j));
