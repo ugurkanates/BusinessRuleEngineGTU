762fc8f658ded94db8f53dfa2e7c3ac91d0db896
bnyorukoglu <bnyorukoglu@gmail.com>
2018-12-18 17:06:49 +0300

    write new method on CNF.java

--- SatSolver/CNF.java
+++ SatSolver/CNF.java
@@ -22,27 +22,35 @@ public class CNF {
 
     public boolean cnfFormat(){
         boolean result=true;
-
-        clearString();
-        if (data.contains(".") == false || data.charAt(0) != '(' || data.charAt(data.length() - 1) != ')') {
-            result = false;
-        }
-        else {
-            // oldIndex was used to follow next clauses.
-            int oldIndex = 0;
-            // result==true should be a condition. When a clause is invalid. input CNF should be invalid.
-            for (int i = oldIndex; i < data.length() && result == true; ++i) {
-                if (data.charAt(i) == '.') {
-                    if (!checkingClause(data.substring(oldIndex, i)))
-                        result = false;
-                    oldIndex = i + 1;
+        try{
+            clearString();
+            if (data.contains(".") == false || data.charAt(0) != '(' || data.charAt(data.length() - 1) != ')') {
+                if (checkingClause(data.substring(0, data.length()))) {
+                    return true;
                 }
-            }
-            if (!checkingClause(data.substring(oldIndex, data.length())))
                 result = false;
+            }
+            else {
+                int oldIndex = 0;
+                
+                for (int i = oldIndex; i < data.length() && result == true; ++i) {
+                    if (data.charAt(i) == '.') {
+                        if (!checkingClause(data.substring(oldIndex, i)))
+                            result = false;
+                        oldIndex = i + 1;
+                    }
+                }
+                if (!checkingClause(data.substring(oldIndex, data.length())))
+                    result = false;
+            }
+        }
+        catch(Exception e){
+            System.out.print(e.getMessage() + " --->");
+            result=false;
         }
         return result;
     }
+
 
     protected boolean checkingClause(String var){
         boolean result=true;
